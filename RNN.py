cd99219331e06022e2875ec98b37b479d532cca6
ThyTab <18038099879@163.com>
2026-03-17 21:43:37 +0800

    循环神经网络

--- RNN.py
+++ RNN.py
@@ -7,14 +7,15 @@ import torch
 import torch.nn as nn
 
 #创建RNN模型
-# 词向量维度   隐藏状态维度   层数
+# 词向量维度   隐藏状态维度   隐藏层层数
 rnn = nn.RNN(input_size=128, hidden_size=256, num_layers=1)
 
-#定义变量，表示输入
-# 句子长度   句子个数   词向量维度
+# 定义变量，表示输入
+# 句子长度   句子个数(batch_size)   词向量维度
 x = torch.randn(5,32,128)
 
-#定义变量，表示上一时刻隐藏状态
+# 定义变量，表示上一时刻隐藏状态
+# 隐藏层层数   句子个数(batch_size)   隐藏状态维度
 h0 = torch.randn(1,32,256)
 
 #调用RNN处理
